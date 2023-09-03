fc702ac3ca109ff1b5adfcdadc6967b5709c9e02
cam <cwan0148@student.monash.edu>
2023-09-03 23:11:07 +1000

    update week 5 and 6 code

---
+++ FIT5032_W5/FIT5032_W5/Controllers/SampleController.cs
@@ -0,0 +1,87 @@
+﻿using FIT5032_W5.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIT5032_W5.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: SampleController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SampleController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SampleController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FormOneViewModel model)
+        {
+            try
+            {
+                String FirstName = model.FirstName;
+                String LastName = model.LastName;
+                ViewBag.FullName = FirstName + " " + LastName;
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SampleController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SampleController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SampleController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SampleController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FIT5032_W5/FIT5032_W5/Models/FormOneViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace FIT5032_W5.Models
+{
+    public class FormOneViewModel
+    {
+        [Required(ErrorMessage ="First Name is required.")]
+        [MinLength(2, ErrorMessage = "First Name must have a minimum length of 2")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
