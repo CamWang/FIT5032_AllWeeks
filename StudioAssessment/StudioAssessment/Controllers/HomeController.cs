49c28e764ab904a4382cbd6eefec045e87ceeb05
cam <cwan0148@student.monash.edu>
2023-09-17 23:41:05 +1000

    studio assement

---
+++ StudioAssessment/StudioAssessment/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudioAssessment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToPage("/Index", new { Message = "Welcome to Ultrasound Appointment System" });
+        }
+    }
+}
