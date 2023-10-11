ffbf31e7eaa81eb6d41147e7ba64ecd0202afdf5
cam <cwan0148@student.monash.edu>
2023-10-11 22:35:46 +1100

    finish week 10

---
+++ FIT5032_W10/FIT5032_W10/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FIT5032_W10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static List<string> values = new List<string>();
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return values;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return values[id];
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            values.Add(value);
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            if (id >= 0 && id < values.Count)
+            {
+                values[id] = value;
+            }
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (id >= 0 && id < values.Count)
+            {
+                values.RemoveAt(id);
+            }
+        }
+    }
+}
